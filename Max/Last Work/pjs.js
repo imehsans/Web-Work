3d9a36f27c17c73d05ffcf838a9120eb9ec57101
FA17-BSE-024 <FA17-BSE-024@cuiatd.edu.pk>
2021-09-01 16:29:42 +0500

    Work start in  MERN

---
+++ Max/Last Work/pjs.js
@@ -0,0 +1,14 @@
+
+// let count =
+		
+ let count = document.getElementById("count-el").innerValue;
+
+
+function countr(){}
+	count = count + 1;
+	document.getElementById("count-el").innerHTML = count;
+}
+
+function reseta(){
+
+}
